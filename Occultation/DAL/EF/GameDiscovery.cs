e66720c83bedcc341e24be7449741c278aafbc3e
Thomas <thomas.lombardi@gmail.com>
2015-05-29 08:13:30 -0500

    Sometime Visual Studio forgets to add a newly created file to source control.  I need to learn command line git.

---
+++ Occultation/DAL/EF/GameDiscovery.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Occultation.DAL.EF
+{
+    public class GameDiscovery
+    {
+        public int Id { get; set; }
+        public int GameId { get; set; }
+        public int DiscoveryId { get; set; }
+        public int SortOrder { get; set; }
+        public bool Revealed { get; set; }
+        public int PlayerId { get; set; }
+        public bool? Claimed { get; set; }
+    }
+}
